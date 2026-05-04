934ba57de750acdf08aa8c41185d1c0b6aee66bf
quintana2427-prog <quintana2427@gmail.com>
2026-05-04 18:01:28 -0500

    + modificacion de cantidad de ciclos

--- ciclo-while.js
+++ ciclo-while.js
@@ -1,12 +1,14 @@
 let intento = 1
-const maximoIntentos = 3
+let ciclo = 1
+const maximoIntentos = 4000000
 
 console.log("Práctica inicial con while")
 console.log("Vamos a repetir un proceso mientras el intento sea menor o igual al máximo.")
 
 while (intento <= maximoIntentos) {
-  console.log("Intento de revisión número:", intento)
+  console.log("Intento de revisión número:", intento, " ciclo, numero;", ciclo)
   intento = intento + 1
+  ciclo = ciclo + 1
 }
 
 console.log("Proceso de revisión finalizado.")
