46233c72a95a7d9bd4940de419cd5455c6234ebd
quintana2427-prog <quintana2427@gmail.com>
2026-05-04 19:06:01 -0500

    modificacion seguimiento-comunitario.js

---
+++ seguimiento-comunitario.js
@@ -0,0 +1,18 @@
+let semana = 1
+const SemanasDeSeguimiento =10
+let propuestasAtendidas = 0
+
+console.log("seguimiento comunal semanal")
+
+while ( semana<= SemanasDeSeguimiento) {
+    console.log ("semana de seguimiento numero;", semana )
+propuestasAtendidas = propuestasAtendidas + 2
+console.log ("propuestas atendidas hasta ahora;", propuestasAtendidas)
+semana = semana + 1
+
+} console.log("seguimiento comunitario finalizado")
+console.log("total de propuestas atendidas;",propuestasAtendidas) 
+
+
+
+
